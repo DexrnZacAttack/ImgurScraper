9d0c744ffe0dea2211e1bc9ce3c9dc83045beed5
ZacAttack <zachariahlehman@gmail.com>
2023-04-28 21:53:51 -0700

    old scraper for usefulness

---
+++ ImgurRedditScraper/README.md
@@ -0,0 +1,3 @@
+This reddit scraper was made by Dexrn and Boreal, and may work better for scraping images, since at the time of making this readme, it is the only one that ONLY scrapes imgur images.
+<br>
+It is VERY slow.
---
+++ ImgurRedditScraper/main.py
@@ -0,0 +1,46 @@
+import praw
+import os
+import re
+from bs4 import BeautifulSoup
+import atexit
+
+# Set up the Reddit instance and the subreddit to scrape
+reddit = praw.Reddit(client_id='vYeGNSLN1VqcxvRvolu4Bw',
+                     client_secret='81SufJbzWWdFLg60cUiupfZegGpz3w',
+                     user_agent='imgurscraper by v1.0 by /u/ZacAttackedYou)')
+subreddit = reddit.subreddit('minecraft')
+
+# Open file for writing
+f = open('links.txt', 'a')
+
+def write_to_file():
+    f.close()
+
+atexit.register(write_to_file)
+
+# Iterate over the submissions in the subreddit
+for submission in subreddit.new(limit=100000):
+    # Check if the submission is a link post and if it links to Imgur
+    if submission.is_self == False and 'imgur.com' in submission.url:
+        # Write the Imgur link to the file
+        f.write(submission.url + '\n')
+
+# Define the query string
+query = "imgur"
+
+for submission in subreddit.search(query, sort="old", limit=1000):
+    if submission.is_self == False and 'imgur.com' in submission.url:
+        f.write(submission.url + '\n')
+    if submission.is_self == True and submission.selftext_html is not None:
+        soup = BeautifulSoup(submission.selftext_html, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href is not None and 'imgur.com' in href:
+                f.write(href + '\n')
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        soup = BeautifulSoup(comment.body_html, 'html.parser')
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if href is not None and 'imgur.com' in href:
+                f.write(href + '\n')
