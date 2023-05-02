26e39af78df60f84398190a93b929cfdafa4e6b4
Boreal <90580455+bor-real@users.noreply.github.com>
2023-05-02 15:45:41 -0700

    Just testing

--- BTWScraper/Python/README.md
+++
@@ -1,3 +0,0 @@
-Scrapes: https://sargunster.com/btwforum/
-
-made by Boreal#9423
---
+++ common/cleanup.py
@@ -0,0 +1,47 @@
+
+def cleanup(filename):
+    
+    proceed = input("Do you want to remove duplicate lines and format the file? (Y/N)").lower()
+
+    if proceed == 'y':
+        with open(filename, 'r+') as file:
+            # Use a set to keep track of unique lines
+            unique_lines = set()
+
+            # Keep track of the number of characters written so far
+            char_count = 0
+
+            # Iterate over each line in the file
+            lines = file.readlines()
+            file.seek(0)
+            file.truncate()
+            for line in lines:
+                # Strip any whitespace from the beginning and end of the line
+                line = line.strip()
+
+                # Only write the line back to the file if it hasn't already been written
+                if line not in unique_lines:
+                    # Add the line to the set of unique lines
+                    unique_lines.add(line)
+
+                    # Add the length of the line to the character count
+                    char_count += len(line)
+
+                    # Write the line to the file
+                    file.write(line + '\n')
+
+                    # If we've written 1000 characters or more, add a blank line
+                    if char_count >= 1000:
+                        file.write('\n')
+
+                        # Reset the character count
+                        char_count = 0
+                else:
+                    # If the line is a duplicate, don't count it towards the character count
+                    continue
+
+            # If the last line didn't end in a newline character, add one to ensure the file ends with a newline
+            if lines[-1][-1] != '\n':
+                file.write('\n')
+    else:
+        print("Operation cancelled.")
